ad80480d660cba94a013c60f7725ec9e96dea2f7
LucasCorbalan <lucas.corbalan.23@gmail.com>
2023-11-10 13:59:06 -0300

    add changewebnotice.js

--- src/components/WebNotice/WebNotice.js
+++ src/components/WebNotice/WebNotice.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row } from "react-bootstrap";
 
 const WebNotice = (props) => {
   const { articles } = props;
   return (
     <div>
       <h2 className="text-center">Noticias</h2>
+      <Row className="my-3 p-2">
       {articles.map((articles) => (
-        <Card style={{ width: "18rem" }}>
+        <Card className="mx-4 my-2">
           <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Subtitle className="mb-2 text-muted">
+            <Card.Subtitle className="mt-3 text-muted">
               {articles.author}
             </Card.Subtitle>
           <Card.Body>
@@ -19,6 +20,7 @@ const WebNotice = (props) => {
           </Card.Body>
         </Card>
       ))}
+      </Row>
     </div>
   );
 };
